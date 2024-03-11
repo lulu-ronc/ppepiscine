b8ee12499a9c801ec54ab279cf6c71b295c5da45
lulu-ronc <ltrol581@gmail.com>
2024-03-11 11:56:29 +0100

    guhjk

---
+++ piscine_jjlm/src/main/java/controlleur/controlleur.java
@@ -0,0 +1,14 @@
+package controlleur;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class controlleur {
+    @FXML
+    private Button achat;
+
+    public void achat(ActionEvent event) {
+
+    }
+}
