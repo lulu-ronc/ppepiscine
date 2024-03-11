560852ce0ab39d53129dc40a8d37130841c74541
jKirida <jimmy.bousardo@aftec.org>
2024-03-11 14:26:21 +0100

    mise a jour par rapport au master
    mise a jour par rapport au master

---
+++ piscine_jjlm/src/main/java/controlleur/controlleur.java
@@ -0,0 +1,15 @@
+package controlleur;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class controlleur {
+	
+	@FXML
+    private Button achat;
+
+    public void achat(ActionEvent event) {
+
+    }
+}
